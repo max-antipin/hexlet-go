d312b458af167a8d7e961522c237826709208180
Max Antipin <max.v.antipin@gmail.com>
2026-02-25 09:02:51 +0300

    Add color

--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/fatih/color"
 	"hexlet-go/greeting"
 )
 
 func main() {
 	fmt.Println(greeting.Get())
+	color.Red("We have red")
+	color.Magenta("And many others ..")
 }
